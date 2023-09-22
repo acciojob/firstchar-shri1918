3acdecee5dfe5c4dc05528a1e07a128b07070deb
Shridhar Savant <93203347+shri1918@users.noreply.github.com>
2023-09-22 11:51:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstChar(text) {
   // your code here
+ if (!text.trim()) {
+        return '';
+    }
+
+    // Loop through the characters in the string
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        // Return the first non-space charactera
+        if (char !== ' ') {
+            return char;
+        }
+    }
+
+    // Return an empty string if no non-space character is found
+    return '';
 }
 
 // Do not change the code below
